37a49aff597a0447c43baf385b7e66a76f4759e6
Nandit <nanditsharma063@gmail.com>
2025-01-16 14:25:56 +0530

    setting inspiration

--- web-app/src/Components/InspirationCards.jsx
+++ web-app/src/Components/InspirationCards.jsx
@@ -7,6 +7,7 @@ const InspirationCards = () => {
     const [activeVideo, setActiveVideo] = useState(null);
     const [runVideo, setRunVideo] = useState(null);
 
+
     const handleMouseEnter = (videoID) => {
         setActiveVideo(videoID);
     }
@@ -21,41 +22,41 @@ const InspirationCards = () => {
       <p className="mt-10 sm:text-5xl text-2xl font-bold mx-auto flex flex-col items-center justify-center">Just Some Examples to Get{" "}</p>
       <p className="mb-10 text-5xl font-bold mx-auto flex flex-col items-center justify-center">You Inspired!</p>
       <div className="flex flex-col sm:flex-row justify-between sm:mx-40 mx-2">
-        {
-          activeVideo ? 
-          (
-              <iframe src={`https://www.youtube.com/embed/${activeVideo}?autoplay=1`} allow="autoplay; encrypted-media" allowFullScreen title="Active Video" className="sm:block hidden w-80 h-[500px]"></iframe>
-          ) 
-          : 
-          <div className="flex flex-col gap-8">
-              <p className="text-5xl font-semibold text-blue-600">Get the results</p>
-              <img src={logo_1} className="sm:block hidden w-80 h-72" alt="" />
-          </div>
-        }
+      {
+        activeVideo ? 
+        (
+            <iframe src={`https://www.youtube.com/embed/${activeVideo}?autoplay=1`} allow="autoplay; encrypted-media" allowFullScreen title="Active Video" className="sm:block hidden w-80 h-[500px]"></iframe>
+        ) 
+        : 
+        <div className="flex flex-col gap-8">
+            <p className="text-5xl font-semibold text-blue-600">Get the results</p>
+            <img src={logo_1} className="sm:block hidden w-80 h-72" alt="" />
+        </div>
+      }
         <div className="body">
             <div className="row">
                 {
                     inspirationCards.slice(0, 3).map((card) => (
-                        <img onMouseLeave={handleMouseLeave} onMouseEnter={() => handleMouseEnter(card.videoID)} src={card.thumbanil} alt="" key={card._id} />
+                        <img onMouseLeave={handleMouseLeave} onMouseEnter={() => handleMouseEnter(card.videoID)} src={card.thumbnail} alt="" key={card._id} />
                     ))
                 }
             </div>
             <div className="row">
                 {
                     inspirationCards.slice(3, 5).map((card) => (
-                        <img onMouseLeave={handleMouseLeave} onMouseEnter={() => handleMouseEnter(card.videoID)} src={card.thumbanil} alt="" key={card._id} />
+                        <img onMouseLeave={handleMouseLeave} onMouseEnter={() => handleMouseEnter(card.videoID)} src={card.thumbnail} alt="" key={card._id} />
                     ))
                 }
             </div>
             <div className="row">
                 {
                     inspirationCards.slice(5, 8).map((card) => (
-                        <img onMouseLeave={handleMouseLeave} onMouseEnter={() => handleMouseEnter(card.videoID)} src={card.thumbanil} alt="" key={card._id} />
+                        <img onMouseLeave={handleMouseLeave} onMouseEnter={() => handleMouseEnter(card.videoID)} src={card.thumbnail} alt="" key={card._id} />
                     ))
                 }
             </div>
         </div>
-        <div className="flex gap-4 my-5 overflow-x-hidden">
+        <div className="flex gap-4 my-5 overflow-x-hidden sm:hidden block">
           {
              inspirationCards.map((item) => (
               <div key={item._id} className="flex-shrink-0">
