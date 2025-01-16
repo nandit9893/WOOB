29d5e0edad0477de493bd15d27d7c8805b099c47
Nandit <nanditsharma063@gmail.com>
2025-01-16 14:13:30 +0530

    set

--- web-app/src/Components/InspirationCards.jsx
+++ web-app/src/Components/InspirationCards.jsx
@@ -5,6 +5,7 @@ import "../index.css";
 
 const InspirationCards = () => {
     const [activeVideo, setActiveVideo] = useState(null);
+    const [runVideo, setRunVideo] = useState(null);
 
     const handleMouseEnter = (videoID) => {
         setActiveVideo(videoID);
@@ -16,7 +17,7 @@ const InspirationCards = () => {
     
 
   return (
-    <div className="flex flex-col sm:mt-96 mt-10 h-[120vh] sm:p-0 p-1">
+    <div className="flex flex-col sm:mt-96 mt-10 h-[100vh] sm:p-0 p-1">
       <p className="mt-10 sm:text-5xl text-2xl font-bold mx-auto flex flex-col items-center justify-center">Just Some Examples to Get{" "}</p>
       <p className="mb-10 text-5xl font-bold mx-auto flex flex-col items-center justify-center">You Inspired!</p>
       <div className="flex flex-col sm:flex-row justify-between sm:mx-40 mx-2">
@@ -54,23 +55,23 @@ const InspirationCards = () => {
                 }
             </div>
         </div>
-        <div className="grid grid-cols-2 gap-4 my-5">
-            {
-              inspirationCards.map((item) => (
-                <div className="gap-4" key={item._id}>
-                  {
-                    activeVideo === item.videoID ?
-                    (
-                      <iframe src={`https://www.youtube.com/embed/${activeVideo}?autoplay=1`} allow="autoplay; encrypted-media" allowFullScreen title="Active Video" className="w-40 h-52 rounded-xl"></iframe>
-                    )
-                    :
-                    (
-                      <img onMouseLeave={handleMouseLeave} onMouseEnter={() => handleMouseEnter(item.videoID)} src={item.thumbanil} className="w-48 h-52 rounded-xl" alt="" />
-                    )
-                  }
-                </div>
-              ))
-            }
+        <div className="flex gap-4 my-5 overflow-x-hidden">
+          {
+             inspirationCards.map((item) => (
+              <div key={item._id} className="flex-shrink-0">
+                {
+                  runVideo === item.videoID ? 
+                  (
+                    <iframe src={`https://www.youtube.com/embed/${runVideo}`} allow="encrypted-media" allowFullScreen title="Active Video" className="w-52 h-80 rounded-xl cursor-pointer"/>
+                  ) 
+                  : 
+                  (
+                    <img src={item.thumbnail} alt="" className="w-52 h-80 rounded-xl cursor-pointer" onClick={() => setRunVideo(item.videoID)} />
+                  )
+                }
+              </div>
+            ))
+          }
         </div>
       </div>
     </div>
